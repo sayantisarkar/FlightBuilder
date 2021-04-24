a96fbaab2c9753405957b175ea32c43b58a25238
Sayanti Sarkar <sayantisarkar088@gmail.com>
2021-04-24 10:05:23 +0400

    Eureka service and edge service created along with master service module(no logic)

---
+++ MasterDataService/src/main/java/com/sayantisarkar/masterDataService/MasterDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sayantisarkar.masterDataService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterDataServiceApplication.class, args);
+	}
+
+}
